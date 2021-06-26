e34af7b11c89bf76aaff23f34f8e44d082f86425
milliwonkim <rldnjs9347@gmail.com>
2021-06-26 18:08:55 +0900

    [chore] add first project configure

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
     return (
         <div style={{ backgroundColor: 'red' }}>
-            <h1>No CRA React Project!!</h1>
+            <h1>No CRA React Project!</h1>
             <div style={{ backgroundColor: 'blue', color: 'white' }}>this works!!!</div>
         </div>
     );
